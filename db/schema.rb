c98fdb7e73d5d59667739beff43a6a65a3c2c6d2
Richard Martin <richardgeorgemartin@gmail.com>
2014-10-01 20:29:49 -0400

    Creating new and index views for products and added url colum to products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930200455) do
+ActiveRecord::Schema.define(version: 20141001142328) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140930200455) do
     t.integer  "price_in_cents"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
 end
